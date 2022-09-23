1c71ae32a84d6a6b920531d07e581150f921ac13
Samuel Song <45842379+samuelsongg@users.noreply.github.com>
2022-09-24 00:59:26 +0800

    Menu added to choose to scrape or print data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -7,58 +8,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import xlsxwriter
-import requests
-import selenium.webdriver.remote.webdriver
-from newspaper import Article
+import pandas as pd
+# import requests
+# import selenium.webdriver.remote.webdriver
+# from newspaper import Article
 # import nltk
 # nltk.download("punkt")
 # nltk.download("stopwords")
-
-#change accordingly
-username = "samuelsong98@hotmail.com"
-password = ""
-software_engineer_url = "https://www.linkedin.com/jobs/search/?currentJobId=3204289656&keywords=software%20engineer&refresh=true"
-cyber_security_specialist_url = "https://www.linkedin.com/jobs/search/?currentJobId=3150477953&keywords=cyber%20security%20specialist&refresh=true"
-data_analyst_url = "https://www.linkedin.com/jobs/search/?currentJobId=3246469376&keywords=data%20analyst&refresh=true"
-excel_list = []
-
-while True:
-    try:
-        job_option = int(input("Please pick an option:\n1. Software Engineer\n2. Cyber Security Specialist\n3. Data Analyst\n"))
-        if job_option in range(1 ,4):
-            break
-    except:
-        pass
-
-if job_option == 1:
-    search_field = software_engineer_url
-    xl_name = "SoftwareEngineer"
-elif job_option == 2:
-    search_field = cyber_security_specialist_url
-    xl_name = "CyberSecuritySpecialist"
-elif job_option == 3:
-    search_field = data_analyst_url
-    xl_name = "DataAnalyst"
-
-while True:
-    try:
-        pages = int(input("How many pages do you want to scrape data from? "))
-        #limit pages to 5 otherwise account/IP might get ban
-        if pages in range(1, 6):
-            break
-    except:
-        pass
-
-#change file path accordingly
-path_to_chromedriver = "C:/Program Files/Google/Chrome/Application/chromedriver.exe"
-browser = webdriver.Chrome(executable_path = path_to_chromedriver)
-browser.get("https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Ffeed%2F%3FdoFeedRefresh%3Dtrue%26nis%3Dtrue&fromSignIn=true&trk=cold_join_sign_in")
-browser.find_element(By.ID, "username").send_keys(username)
-browser.find_element(By.ID, "password").send_keys(password)
-browser.find_element(By.CLASS_NAME, "login__form_action_container").submit()
-WebDriverWait(browser, 60).until(EC.visibility_of_element_located((By.CLASS_NAME, "scaffold-layout__sidebar")))
-browser.get(search_field)
-action = ActionChains(browser)
 
 def data_scraping():
     WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "job-card-list__title")))
@@ -147,6 +103,17 @@ def data_scraping():
         print(f"\t{'Job Posted Date:' : <18}{posted_date}")
         print(f"\t{'Applicants:' : <18}{applicants}\n\n")
 
+def page_navigator():
+    for y in range(pages-1):
+        if y + 2 > pages:
+            break
+        else:
+            y_fm = "Page " + str(y + 2)
+            p = browser.find_element(By.XPATH, "//button[@aria-label='{}']/span".format(y_fm))
+            action.move_to_element(p).click().perform()
+            time.sleep(1.5)
+            data_scraping()
+
 def excel_write():
     workbook = xlsxwriter.Workbook(f'{xl_name}.xlsx')
     worksheet = workbook.add_worksheet()
@@ -159,19 +126,87 @@ def excel_write():
 
     workbook.close()
 
-def page_navigator():
-    for y in range(pages-1):
-        if y + 2 > pages:
-            break
-        else:
-            y_fm = "Page " + str(y + 2)
-            p = browser.find_element(By.XPATH, "//button[@aria-label='{}']/span".format(y_fm))
-            action.move_to_element(p).click().perform()
-            time.sleep(1.5)
-            data_scraping()
+def excel_print():
+    try:
+        pd.set_option('display.max_colwidth', 200)
+        pd.set_option("display.expand_frame_repr", False)
+        pd.set_option("display.max_rows", None)
+        df = pd.read_excel(f"{xl_name}.xlsx")
+        print(df)
+    except:
+        print("No such file.")
 
-i = 0
-data_scraping()
-page_navigator()
-excel_write()
-browser.close()
+#change accordingly
+username = "samuelsong98@hotmail.com"
+password = ""
+software_engineer_url = "https://www.linkedin.com/jobs/search/?currentJobId=3204289656&keywords=software%20engineer&refresh=true"
+cyber_security_specialist_url = "https://www.linkedin.com/jobs/search/?currentJobId=3150477953&keywords=cyber%20security%20specialist&refresh=true"
+data_analyst_url = "https://www.linkedin.com/jobs/search/?currentJobId=3246469376&keywords=data%20analyst&refresh=true"
+excel_list = []
+
+while True:
+    try:
+        menu_option = int(input("Please select an option:\n1. Scrape data\n2. Print data\n3. Exit\n"))
+        if menu_option in range(1, 4):
+            if menu_option == 3:
+                sys.exit()
+            else:
+                break
+    except SystemExit:
+        sys.exit()
+    except:
+        pass
+
+
+while True:
+    try:
+        job_option = int(input("Please select an option:\n1. Software Engineer\n2. Cyber Security Specialist\n3. Data Analyst\n"))
+        if job_option in range(1, 4):
+            break
+    except:
+        pass
+
+if job_option == 1:
+    search_field = software_engineer_url
+    xl_name = "SoftwareEngineer"
+elif job_option == 2:
+    search_field = cyber_security_specialist_url
+    xl_name = "CyberSecuritySpecialist"
+elif job_option == 3:
+    search_field = data_analyst_url
+    xl_name = "DataAnalyst"
+
+if menu_option == 1:
+    while True:
+        try:
+            pages = int(input("How many pages do you want to scrape data from? "))
+            #limit pages to 5 otherwise account/IP might get ban
+            if pages in range(1, 6):
+                break
+
+        except:
+            pass
+
+    # change file path accordingly
+    path_to_chromedriver = "C:/Program Files/Google/Chrome/Application/chromedriver.exe"
+    browser = webdriver.Chrome(executable_path=path_to_chromedriver)
+    browser.get(
+        "https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Ffeed%2F%3FdoFeedRefresh%3Dtrue%26nis%3Dtrue&fromSignIn=true&trk=cold_join_sign_in")
+    browser.find_element(By.ID, "username").send_keys(username)
+    browser.find_element(By.ID, "password").send_keys(password)
+    browser.find_element(By.CLASS_NAME, "login__form_action_container").submit()
+    WebDriverWait(browser, 60).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "scaffold-layout__sidebar")))
+    browser.get(search_field)
+    action = ActionChains(browser)
+    i = 0
+
+    data_scraping()
+    page_navigator()
+    excel_write()
+    browser.close()
+
+if menu_option == 2:
+    excel_print()
+
+
--- test.py
+++ test.py
@@ -1,3 +1,35 @@
-y_fm = "Page " + str(1)
+import xlsxwriter
+import pandas as pd
+import openpyxl
 
-print(y_fm)
+while True:
+    try:
+        job_option = int(input("Please select an option:\n1. Software Engineer\n2. Cyber Security Specialist\n3. Data Analyst\n"))
+        if job_option in range(1, 4):
+            break
+    except:
+        pass
+
+if job_option == 1:
+    xl_name = "SoftwareEngineer"
+elif job_option == 2:
+    xl_name = "CyberSecuritySpecialist"
+elif job_option == 3:
+    xl_name = "DataAnalyst"
+
+
+# def excel_print():
+#     workbook = openpyxl.load_workbook(f"{xl_name}.xlsx")
+#     worksheet = workbook.active
+#     print(worksheet.cell(row=1, column=1).value)
+
+def excel_print():
+    try:
+        pd.set_option('display.max_colwidth', 200)
+        pd.set_option("display.expand_frame_repr", False)
+        pd.set_option("display.max_rows", None)
+        df = pd.read_excel(f"{xl_name}.xlsx")
+        print(df)
+    except:
+        print("No such file.")
+excel_print()
